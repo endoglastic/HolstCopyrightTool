9b4492062ade2b143fe5a4e172cf46b6f63c4c1d
Sam Holst <sawohol@gmail.com>
2017-07-25 20:05:41 -0600

    changed syntax

--- lib/holst_copyright_tool/renderer.rb
+++ lib/holst_copyright_tool/renderer.rb
@@ -1,7 +1,9 @@
 module HolstCopyrightTool
   class Renderer
     def self.copyright name, msg
-      "<p class=\"site-copyright\">&copy; #{Time.now.year} | #{name} #{msg}</p>".html_safe
+      # "<p class=\"site-copyright\">&copy; #{Time.now.year} | #{name} #{msg}</p>".html_safe
+      rights = "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}"
+      content_tag(:p, rights, class: "site-copyright")
     end
   end
 end
