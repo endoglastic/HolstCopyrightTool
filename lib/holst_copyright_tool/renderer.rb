bd0a35cc95330bf91abfd6cccbc673f42f98e519
Sam Holst <sawohol@gmail.com>
2017-07-25 20:08:36 -0600

    reversed syntax

--- lib/holst_copyright_tool/renderer.rb
+++ lib/holst_copyright_tool/renderer.rb
@@ -1,9 +1,7 @@
 module HolstCopyrightTool
   class Renderer
     def self.copyright name, msg
-      # "<p class=\"site-copyright\">&copy; #{Time.now.year} | #{name} #{msg}</p>".html_safe
-      rights = "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}"
-      content_tag(:p, rights, class: "site-copyright")
+      "<p class=\"site-copyright\">&copy; #{Time.now.year} | #{name} #{msg}</p>".html_safe
     end
   end
 end
